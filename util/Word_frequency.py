d42ef6b79100ef331007de161266bcddce92e865
Damotorie <parksjin01@gmail.com>
2017-01-27 13:20:02 +0900

    Create Word_frequency.py

---
+++ util/Word_frequency.py
@@ -0,0 +1,13 @@
+def word_frequency(file):
+    word = {}
+    ascii = string.ascii_letters
+    with open(file, 'r') as f:
+        text = f.read()
+        for char in text:
+            char = char.lower()
+            if char in ascii:
+                try:
+                    word[char] += 1
+                except:
+                    word[char] = 1
+    return word
