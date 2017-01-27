d41aaf910d58143bb0e5ac22b7cd6ea9a3aa832c
Damotorie <parksjin01@gmail.com>
2017-01-27 15:32:12 +0900

    Update Vigenere.py

--- Encryption_Algorithm/Vigenere.py
+++ Encryption_Algorithm/Vigenere.py
@@ -1,5 +1,6 @@
 import random
 import string
+import os
 
 def encryption(file_name, key_len=10, key=[]):
     alphabet = string.ascii_letters
@@ -21,14 +22,11 @@ def encryption(file_name, key_len=10, key=[]):
             if alpha in alphabet:
                 alpha = ord(alpha) + key[key_idx%key_len]
                 key_idx += 1
-                # if alpha > ord('Z') and alpha < ord('a'):
-                #     alpha = alpha - ord('Z') + ord('A')
                 if alpha > ord('z'):
                     alpha = alpha - ord('z') + ord('a')
                 f.write(chr(alpha))
             else:
                 f.write(alpha)
-        print key,'is your key. You have to keep it.'
         return key
 
 def decryption(file_name, key):
@@ -42,12 +40,52 @@ def decryption(file_name, key):
             if alpha in alphabet:
                 alpha = ord(alpha) - key[key_idx%key_len]
                 key_idx += 1
-                # if alpha > ord('Z') and alpha < ord('a'):
-                #     alpha = alpha + ord('z') - ord('a')
                 if alpha < ord('a'):
                     alpha = alpha + ord('z') - ord('a')
                 f.write(chr(alpha))
             else:
                 f.write(alpha)
-key = encryption('plain.txt', 1, [2])
-decryption('plain_cipher.txt', key)
+
+def ECB_Mode_Encryption(file_name, key = [], key_len=10, block_size=1, algorithm="Vigenere"):
+    res = ''
+    with open(file_name, 'r') as f:
+        data = f.read()
+    if len(data)%block_size != 0:
+        data += 'z'*(block_size-len(data)%block_size)
+    if key == []:
+        key = []
+        for _ in range(key_len):
+            key.append(random.randrange(1, 25))
+    else:
+        key_len = len(key)
+    key_idx = 0
+    for idx in range(len(data)/block_size-1):
+        with open('tmp', 'w') as f:
+            f.write(data[idx*block_size:(idx+1)*block_size])
+        encryption('tmp', 1, [key[key_idx%key_len]])
+        with open('_cipher.txt', 'r') as f:
+            res += f.read()
+        key_idx += 1
+    os.remove('tmp')
+    os.remove('_cipher.txt')
+    with open(file_name[:-4]+'_cipher.txt', 'w') as f:
+        f.write(res)
+    return key
+
+def ECB_Mode_Decryption(file_name, key = [], key_len=10, block_size=1, algorithm="Vigenere"):
+    res = ''
+    with open(file_name, 'r') as f:
+        data = f.read()
+    key_len = len(key)
+    key_idx = 0
+    for idx in range(len(data)/block_size-1):
+        with open('tmp_cipher.txt', 'w') as f:
+            f.write(data[idx*block_size:(idx+1)*block_size])
+        decryption('tmp_cipher.txt', [key[key_idx%key_len]])
+        with open('tmp_plain.txt', 'r') as f:
+            res += f.read().rstrip('z')
+        key_idx += 1
+        os.remove('tmp_cipher.txt')
+        os.remove('tmp_plain.txt')
+    with open(file_name[:file_name.index('_cipher.txt')]+"_plain.txt", 'w') as f:
+        f.write(res)
