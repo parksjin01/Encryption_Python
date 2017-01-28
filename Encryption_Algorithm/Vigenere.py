4937b360cf274723eba52ada23c3ecaec49a07e9
Damotorie <parksjin01@gmail.com>
2017-01-28 21:00:12 +0900

    Update Vigenere.py

--- Encryption_Algorithm/Vigenere.py
+++ Encryption_Algorithm/Vigenere.py
@@ -1,6 +1,7 @@
 import random
 import string
 import os
+import Convert
 
 def encryption(file_name, key_len=10, key=[]):
     alphabet = string.ascii_letters
@@ -46,12 +47,12 @@ def decryption(file_name, key):
             else:
                 f.write(alpha)
 
-def ECB_Mode_Encryption(file_name, key = [], key_len=10, block_size=1, algorithm="Vigenere"):
+def ECB_Mode_Encryption(file_name, key = [], key_len=10, block_size=1):
     res = ''
     with open(file_name, 'r') as f:
         data = f.read()
     if len(data)%block_size != 0:
-        data += 'z'*(block_size-len(data)%block_size)
+        data += '='*(block_size-len(data)%block_size)
     if key == []:
         key = []
         for _ in range(key_len):
@@ -59,7 +60,7 @@ def ECB_Mode_Encryption(file_name, key = [], key_len=10, block_size=1, algorithm
     else:
         key_len = len(key)
     key_idx = 0
-    for idx in range(len(data)/block_size-1):
+    for idx in range(len(data)/block_size):
         with open('tmp', 'w') as f:
             f.write(data[idx*block_size:(idx+1)*block_size])
         encryption('tmp', 1, [key[key_idx%key_len]])
@@ -72,20 +73,82 @@ def ECB_Mode_Encryption(file_name, key = [], key_len=10, block_size=1, algorithm
         f.write(res)
     return key
 
-def ECB_Mode_Decryption(file_name, key = [], key_len=10, block_size=1, algorithm="Vigenere"):
+def ECB_Mode_Decryption(file_name, key = [], key_len=10, block_size=1):
     res = ''
     with open(file_name, 'r') as f:
         data = f.read()
     key_len = len(key)
     key_idx = 0
-    for idx in range(len(data)/block_size-1):
+    for idx in range(len(data)/block_size):
         with open('tmp_cipher.txt', 'w') as f:
             f.write(data[idx*block_size:(idx+1)*block_size])
         decryption('tmp_cipher.txt', [key[key_idx%key_len]])
         with open('tmp_plain.txt', 'r') as f:
-            res += f.read().rstrip('z')
+            res += f.read().rstrip('=')
         key_idx += 1
         os.remove('tmp_cipher.txt')
         os.remove('tmp_plain.txt')
     with open(file_name[:file_name.index('_cipher.txt')]+"_plain.txt", 'w') as f:
+        f.write(res)
+
+def CBC_Mode_Encryption(file_name, key = [], key_len = 10, block_size=1):
+    res = ''
+    init_vec = random.randrange(1, 2**(block_size*8))
+    print init_vec
+    cur_vec = init_vec
+    with open(file_name, 'r') as f:
+        data = f.read()
+    if len(data)%block_size != 0:
+        data += '='*(block_size-len(data)%block_size)
+    if key == []:
+        key = []
+        for _ in range(key_len):
+            key.append(random.randrange(1, 25))
+    else:
+        key_len = len(key)
+    key_idx = 0
+    for idx in range(len(data)/block_size):
+        tmp = int(Convert.to_bin(data[idx*block_size:(idx+1)*block_size]), 2)
+        tmp ^= cur_vec
+        tmp = bin(tmp)[2:]
+        tmp = '0'*(8*block_size-len(tmp))+tmp
+        tmp = Convert.to_str(tmp)
+        temp = ''
+        for char in tmp:
+            char = ord(char)+key[key_idx%key_len]
+            temp += chr(char%256)
+        key_idx+=1
+        cur_vec = int(Convert.to_bin(temp), 2)
+        res += temp
+    with open(file_name[:-4]+"_cipher.txt", 'wb') as f:
+        f.write(res)
+
+    return (init_vec, key)
+
+def CBC_Mode_Decryption(file_name, key, init_vec, block_size=1):
+    res = ''
+    cur_vec = init_vec
+    with open(file_name, 'rb') as f:
+        data = f.read()
+    key_len = len(key)
+    key_idx = 0
+    for idx in range(len(data)/block_size):
+        temp = data[idx*block_size:(idx+1)*block_size]
+        temp = Convert.to_bin(temp)
+        temp = int(temp, 2)
+        tmp = ''
+        for char in data[idx*block_size:(idx+1)*block_size]:
+            char = ord(char)-key[key_idx%key_len]
+            if char < 0:
+                char += 256
+            tmp += chr(char)
+        tmp = int(Convert.to_bin(tmp), 2)
+        tmp ^= cur_vec
+        tmp = bin(tmp)[2:]
+        tmp = '0'*(block_size*8-len(tmp))+tmp
+        tmp = Convert.to_str(tmp)
+        key_idx+=1
+        res += tmp
+        cur_vec = temp
+    with open(file_name[:file_name.index('_cipher.txt')]+'_plain.txt', 'w') as f:
         f.write(res)
